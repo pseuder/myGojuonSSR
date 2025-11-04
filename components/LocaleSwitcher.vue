219a9ed368637b306079650375993fd4e44883cf
pseuder <iop890520@gmail.com>
2025-11-04 21:35:20 +0800

    減少多國語系

--- components/LocaleSwitcher.vue
+++ components/LocaleSwitcher.vue
@@ -6,12 +6,8 @@
       :default-active="currentLocale"
       @select="handleSelect"
     >
-      <el-menu-item :index="LOCALE_TW">繁體中文</el-menu-item>
-      <el-menu-item :index="LOCALE_CN">简体中文</el-menu-item>
-      <el-menu-item :index="LOCALE_HK">香港粵語</el-menu-item>
+      <el-menu-item :index="LOCALE_TW">中文</el-menu-item>
       <el-menu-item :index="LOCALE_EN">English</el-menu-item>
-      <el-menu-item :index="LOCALE_MY">Melayu</el-menu-item>
-      <el-menu-item :index="LOCALE_VN">Tiếng Việt</el-menu-item>
     </el-menu>
 
     <template #reference>
@@ -33,11 +29,7 @@ const { locale } = useI18n();
 const currentLocale = computed(() => locale.value);
 
 const LOCALE_TW = "zh-TW";
-const LOCALE_CN = "CN";
-const LOCALE_HK = "HK";
 const LOCALE_EN = "en-US";
-const LOCALE_MY = "MY";
-const LOCALE_VN = "VN";
 
 const emit = defineEmits(["update:locale"]);
 
